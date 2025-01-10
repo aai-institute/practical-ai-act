5ddee781e483e1c7921f10ba09264a04ab571fda
Kristof Schroeder <kristof_schroeder@web.de>
2025-01-10 21:10:14 +0100

    Add docstrings to data provider class

--- src/adult/data.py
+++ src/adult/data.py
@@ -1,5 +1,6 @@
 from pathlib import Path
 
+import pandas as pd
 from sensai import InputOutputData
 from ucimlrepo import fetch_ucirepo
 from sensai.util.cache import pickle_cached
@@ -60,6 +61,16 @@ CLASS_POSITIVE = ">50K"
 
 
 class AdultData:
+    """
+    Data provider for the [`Census Income` dataset](https://archive.ics.uci.edu/dataset/2/adult) from UCI repository.
+    Also known as `Adult` dataset. The provider loads the data using the [`ucimlrepo`](https://github.com/uci-ml-repo/ucimlrepo) package
+    and optionally caches the data using the `pickle` module.
+
+    Args:
+        data_path: Optional directory to cache the downloaded result.
+            If not provided, the data will be downloaded from the UCI repository directly.
+    """
+
     def __init__(self, data_path: str | Path | None = None):
         self._data_path = data_path
 
@@ -70,11 +81,18 @@ class AdultData:
 
         self._data = loading_func(id=2)
 
-    def load_data_frame(self):
+    def load_data_frame(self) -> pd.DataFrame:
+        """
+        Load the data as a pandas DataFrame.
+        """
         df = self._data.data.original
         df.loc[:, COL_TARGET] = df[COL_TARGET].map(lambda x: x.rstrip("."))
         return df
 
-    def load_input_output_data(self):
+    def load_input_output_data(self) -> InputOutputData:
+        """
+        Load the data as an `InputOutputData` object used in the
+        [`sensai`](https://github.com/opcode81/sensAI) package.
+        """
         all_df = self.load_data_frame()
         return InputOutputData(all_df.drop(columns=[COL_TARGET]), all_df[[COL_TARGET]])
