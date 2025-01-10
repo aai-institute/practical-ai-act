639905eaca1beaf655134147805c5f790fd88e6e
Kristof Schroeder <kristof_schroeder@web.de>
2025-01-10 14:42:30 +0100

    Extend config

--- config.py
+++ config.py
@@ -3,7 +3,10 @@ from pathlib import Path
 
 _config_file_folder = os.path.dirname(os.path.abspath(__file__))
 DATA_SUBFOLDER = Path(_config_file_folder) / "data"
+RESULT_SUBFOLDER = Path(_config_file_folder) / "results"
+MLFLOW_SUBFOLDER = Path(_config_file_folder) / "mlruns"
 
 FILE_NAME = DATA_SUBFOLDER / "german_credit_data"
 FILE_NAME_ADULT = DATA_SUBFOLDER / "adult_data"
+
 
