c804484b62479c353ec441b47e1c31a3ad1a4b08
Nicholas Junge <nicholas.junge@web.de>
2025-01-24 09:08:26 +0100

    pipeline: Add sklearn-based preprocessing steps
    Copies the content from `preprocessing.py`, as column transformers instead of sensai's
    `ColumnValueMapping`s.

--- src/adult/pipeline.py
+++ src/adult/pipeline.py
@@ -2,7 +2,15 @@
 
 import typing
 import mlflow
+import pandas as pd
 import sklearn.pipeline
+from functools import partial
+from sklearn.compose import ColumnTransformer
+from sklearn.pipeline import Pipeline
+from sklearn.preprocessing import FunctionTransformer
+
+from adult.data import AdultData
+from adult.preprocessing import WorkClassMapping, EducationMapping
 
 if typing.TYPE_CHECKING:
     # Work around lazy imports in mlflow main module
@@ -29,7 +37,180 @@ def setup_mlflow():
     mlflow.sklearn.autolog()
 
 
+def _fill_na(df: pd.DataFrame, cols: str | list[str], fill_value: typing.Any):
+    if cols:
+        df.loc[:, cols] = df[cols].fillna(fill_value)
+    else:
+        df.fillna(fill_value, inplace=True)
+    return df
+
+
+class NAFiller(ColumnTransformer):
+    def __init__(self, columns: str | list[str], fill_value: typing.Any):
+        mapping = FunctionTransformer(partial(_fill_na, columns=columns, fill_value=fill_value))
+        super().__init__([("fill_na", mapping, columns)], remainder="keep")
+
+
+class ColumnMap(ColumnTransformer):
+    def __init__(self, column: str, mapping: dict):
+        mapping = FunctionTransformer(lambda s: s.map(mapping))
+        super().__init__([(column, mapping, column)], remainder="keep")
+
+
+class NativeCountryMapping(ColumnTransformer):
+    UNKNOWN_DEFAULT = "Unknown"
+    OTHER_DEFAULT = "Other"
+    COUNTRY_MAPPING = {"United-States": "US", "?": UNKNOWN_DEFAULT}
+
+    def __init__(self):
+        # TODO (nicholasjng): This is hacky, please improve.
+        def _transform(df):
+            def reduce_native_country(country):
+                if country in {self.UNKNOWN_DEFAULT, self.OTHER_DEFAULT, "US"}:
+                    return country
+
+                return self.COUNTRY_MAPPING.get(country, self.OTHER_DEFAULT)
+
+            df.loc[:, AdultData.Column.NATIVE_COUNTRY] = df[
+                AdultData.Column.NATIVE_COUNTRY
+            ].apply(reduce_native_country)
+
+        super().__init__([("native_country", _transform, "native_country")], remainder="keep")
+
+
+class WorkClassMapping(ColumnMap):
+    UNKNOWN_DEFAULT = "Unknown"
+    WORKCLASS_MAPPING = {
+        "Private": "Private",
+        "Self-emp-not-inc": "Self-employed",
+        "Self-emp-inc": "Self-employed",
+        "Local-gov": "Government",
+        "State-gov": "Government",
+        "Federal-gov": "Government",
+        "Without-pay": "Unemployed",
+        "Never-worked": "Unemployed",
+        "?": UNKNOWN_DEFAULT,
+    }
+
+    def __init__(self):
+        super().__init__(AdultData.Column.WORK_CLASS,self.WORKCLASS_MAPPING)
+
+
+class OccupationMapping(ColumnMap):
+    UNKNOWN_DEFAULT = "Unknown"
+    OCCUPATION_MAPPING = {
+        "Adm-clerical": "White Collar",
+        "Armed-Forces": "Military",
+        "Craft-repair": "Blue Collar",
+        "Exec-managerial": "White Collar",
+        "Farming-fishing": "Blue Collar",
+        "Handlers-cleaners": "Blue Collar",
+        "Machine-op-inspct": "Blue Collar",
+        "Other-service": "Service",
+        "Priv-house-serv": "Service",
+        "Prof-specialty": "White Collar",
+        "Protective-service": "Service",
+        "Sales": "White Collar",
+        "Tech-support": "White Collar",
+        "Transport-moving": "Blue Collar",
+        "?": UNKNOWN_DEFAULT,
+    }
+
+    def __init__(self):
+        super().__init__(
+            AdultData.Column.EDUCATION,
+            self.OCCUPATION_MAPPING,
+        )
+
+
+class EducationMapping(ColumnMap):
+    EDUCATION_MAPPING = {
+        "Preschool": "No formal education",
+        "1st-4th": "No formal education",
+        "5th-6th": "No formal education",
+        "7th-8th": "No formal education",
+        "9th": "Some High School",
+        "10th": "Some High School",
+        "11th": "Some High School",
+        "12th": "Some High School",
+        "HS-grad": "High School Graduate",
+        "Some-college": "Collage/Associate Degree",
+        "Bachelors": "Bachelor's Degree",
+        "Masters": "Advanced Degree",
+        "Doctorate": "Advanced Degree",
+        "Prof-school": "Advanced Degree",
+        "Assoc-acdm": "Assoc-acdm",
+        "Assoc-voc": "Assoc-voc",
+    }
+
+    def __init__(self):
+        super().__init__(AdultData.Column.EDUCATION, self.EDUCATION_MAPPING)
+
+
+class MaritalStatusMapping(ColumnMap):
+    MARITAL_STATUS_MAPPING = {
+        "Married-civ-spouse": "Married",
+        "Married-spouse-absent": "Married",
+        "Married-AF-spouse": "Married",
+        "Never-married": "Never Married",
+        "Divorced": "Previously Married",
+        "Separated": "Previously Married",
+        "Widowed": "Previously Married",
+    }
+
+    def __init__(self):
+        super().__init__(AdultData.Column.MARITAL_STATUS, self.MARITAL_STATUS_MAPPING)
+
+
+class RelationShipMapping(ColumnMap):
+    RELATIONSHIP_MAPPING = {
+        "Wife": "Spouse",
+        "Husband": "Spouse",
+        "Own-Child": "Child",
+        "Other-relative": "Other Relative",
+        "Not-in-family": "Not family",
+        "Unmarried": "Not family",
+    }
+
+    def __init__(self):
+        super().__init__(AdultData.Column.RELATIONSHIP, self.RELATIONSHIP_MAPPING)
+
+
+class RaceMapping(ColumnMap):
+    RACE_MAPPING = {
+        "White": "White",
+        "Black": "Other",
+        "Asian-Pac-Islander": "Other",
+        "Amer-Indian-Eskimo": "Other",
+        "Other": "Other",
+    }
+
+    def __init__(self):
+        super().__init__(AdultData.Column.RACE, self.RACE_MAPPING)
+
+
 def cps_asec_model():
-    # TODO(nicholasjng): Add steps.
-    p = sklearn.pipeline.Pipeline([])
+
+    p = sklearn.pipeline.Pipeline([
+        WorkClassMapping(),
+        NAFiller(
+            columns=AdultData.Column.WORK_CLASS,
+            fill_value=WorkClassMapping.UNKNOWN_DEFAULT,
+        ),
+        EducationMapping(),
+        MaritalStatusMapping(),
+        OccupationMapping(),
+        NAFiller(
+            columns=AdultData.Column.OCCUPATION,
+            fill_value=OccupationMapping.UNKNOWN_DEFAULT,
+        ),
+        RelationShipMapping(),
+        RaceMapping(),
+        NativeCountryMapping(),
+        NAFiller(
+            columns=AdultData.Column.NATIVE_COUNTRY,
+            fill_value=NativeCountryMapping.UNKNOWN_DEFAULT,
+        ),
+    ])
+    # TODO(nicholasjng): Add model / other steps.
     return p
