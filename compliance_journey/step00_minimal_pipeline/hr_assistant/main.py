f0262c6a676bc55efec44d23dc7ff4bc91d89607
Kristof Schroeder <kristof_schroeder@web.de>
2025-02-05 21:10:13 +0100

    Clean main file of fastAPI app

--- compliance_journey/step00_minimal_pipeline/hr_assistant/main.py
+++ compliance_journey/step00_minimal_pipeline/hr_assistant/main.py
@@ -1,25 +1,12 @@
-import os
-from pathlib import Path
-
 from fastapi import FastAPI, Request
-from pydantic import BaseModel, conint, PositiveFloat
 import pandas as pd
 import mlflow
-
-from config import MLFLOW_SUBFOLDER
-
-# class PredictionInput(BaseModel):
-#    A_AGE: conint(ge=16)
-#    HRSWK: PositiveFloat
-#    FNLWGT: PositiveFloat
 
 
 app = FastAPI()
 
-# MLFLOW_TRACKING_URI = os.getenv("MLFLOW_TRACKING_URI", "http://localhost:5000")
-# MODEL_URI = os.getenv("MODEL_URI", "models:/my_model/Production")
 
-MODEL_URI = "runs:/d9eb179e44324ecc9446b51e612f78c7/asec_model"
+MODEL_URI = "runs:/24581578617d4fbca2e153ac070271b8/asec_model"
 MLFLOW_TRACKING_URI = "/Users/kristof/Projects/twai-pipeline/compliance_journey/step00_minimal_pipeline/mlruns"
 print(MLFLOW_TRACKING_URI)
 
