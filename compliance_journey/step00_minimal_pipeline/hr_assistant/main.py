3c3b820d4e05e394414d22fc6d0972ffcba03db8
Kristof Schroeder <kristof_schroeder@web.de>
2025-02-06 18:04:39 +0100

    Add evaluation module

--- compliance_journey/step00_minimal_pipeline/hr_assistant/main.py
+++ compliance_journey/step00_minimal_pipeline/hr_assistant/main.py
@@ -1,26 +1,28 @@
-from fastapi import FastAPI, Request
-import pandas as pd
+from contextlib import asynccontextmanager
+from fastapi import FastAPI
 import mlflow
 
+from hr_assistant_app import api
 
-app = FastAPI()
 
-
-MODEL_URI = "runs:/2dc7d8653cff4a8ab4219bb7fcd97a7f/asec_model"
+MODEL_URI = "runs:/a62dbc9aff7040598b9b1b66d9b786f3/asec_model"
 MLFLOW_TRACKING_URI = "/Users/kristof/Projects/twai-pipeline/compliance_journey/step00_minimal_pipeline/mlruns"
-print(MLFLOW_TRACKING_URI)
-
 mlflow.set_tracking_uri(MLFLOW_TRACKING_URI)
 
-model = mlflow.sklearn.load_model(MODEL_URI)
+
+def _load_model(model_uri: str) -> mlflow.sklearn.Model:
+    return mlflow.sklearn.load_model(model_uri)
 
 
-@app.get("/info/")
-def model_info():
-    return str(model)
+@asynccontextmanager
+async def lifespan(app: FastAPI):
+    app.state.model = _load_model(MODEL_URI)
+    yield
 
 
-@app.post("/predict/")
-async def predict(request: Request):
-    data = pd.DataFrame.from_records([await request.json()])
-    return model.predict(data).tolist()
+
+app = FastAPI(lifespan=lifespan)
+
+app.include_router(api.predict_router, prefix="/model")
+app.include_router(api.info_router, prefix="model")
+
