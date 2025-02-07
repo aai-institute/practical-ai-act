cbb50847209a500b88d165e84d1e3ad362b02e01
Kristof Schroeder <kristof_schroeder@web.de>
2025-02-07 18:33:20 +0100

    Delete steps folder

--- compliance_journey/step00_minimal_pipeline/hr_assistant/api/info.py
+++
@@ -1,9 +0,0 @@
-from fastapi import APIRouter
-from hr_assistant.dependencies.models import ModelDependency
-
-router = APIRouter(tags=["model"])
-
-
-@router.get("/info")
-def model_info(model: ModelDependency):
-    return str(model)
--- compliance_journey/step01_logging/hr_assistant/config.py
+++
@@ -1,1 +0,0 @@
-MODEL_URI = "models:/xgboost-classifier/latest"
