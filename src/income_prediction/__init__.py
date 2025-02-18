a5fc1146430f5097498a8df8a0f7f81ce8589654
Kristof Schroeder <kristof_schroeder@web.de>
2025-02-18 09:58:47 +0100

    Remove obsolete code

--- src/income_prediction/__init__.py
+++ src/income_prediction/__init__.py
@@ -11,10 +11,14 @@ from income_prediction.io_managers.csv_fs_io_manager import CSVFSIOManager
 from income_prediction.resources.configuration import Config
 from income_prediction.resources.mlflow_session import MlflowSession
 
+from income_prediction.resources.model_factory import XGBClassifierFactory
+from asec.model_factory import build_pipeline
+
 config = Config()
 mlflow_session = MlflowSession(
     tracking_url=config.mlflow_tracking_url, experiment=config.mlflow_experiment
 )
+model_factory = XGBClassifierFactory(random_state=config.random_state)
 
 definitions = Definitions(
     assets=[
@@ -28,5 +32,6 @@ definitions = Definitions(
         "config": config,
         "mlflow_session": mlflow_session,
         "csv_io_manager": CSVFSIOManager(base_dir=config.data_dir),
+        "model_factory": model_factory
     },
 )
