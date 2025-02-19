d4883f970d5f5730dcaf4380c0e17e8db53e5528
Adrian Rumpold <a.rumpold@gmail.com>
2025-02-19 10:29:09 +0100

    feat: Introduce LakeFS I/O manager
    The manager stores Pandas DataFrames a Parquet files in
    a lakeFS repository, based on the Dagster UPath I/O mgr.

---
+++ src/income_prediction/io_managers/lakefs.py
@@ -0,0 +1,39 @@
+import dagster as dg
+import lakefs_spec.util
+import pandas as pd
+from dagster import InputContext, OutputContext
+from upath import UPath
+
+
+class LakeFSIOManager(dg.UPathIOManager):
+    def load_from_path(self, context: InputContext, path: "UPath") -> pd.DataFrame:
+        return pd.read_parquet(
+            path.with_suffix(".parquet"),
+            storage_options=path.storage_options,
+        )
+
+    def dump_to_path(
+        self, context: OutputContext, obj: pd.DataFrame, path: "UPath"
+    ) -> None:
+        repo, ref, resource = lakefs_spec.util.parse(path.path)
+        with path.fs.transaction(
+            repository=repo,
+            base_branch=ref,
+        ) as tx:
+            path.mkdir(parents=True, exist_ok=True)
+
+            # Need to add the file to the transaction branch
+            # the tx merge will apply it to the target branch.
+            ephemeral_path = f"lakefs://{repo}/{tx.branch.id}/{resource}.parquet"
+            obj.to_parquet(
+                ephemeral_path,
+                storage_options=path.storage_options,
+            )
+
+            tx.commit(
+                message=f"Add data from {context.asset_key.path}",
+                metadata={
+                    "dagster.run_id": str(context.run_id),
+                    "dagster.asset_key": str(context.asset_key.path),
+                },
+            )
