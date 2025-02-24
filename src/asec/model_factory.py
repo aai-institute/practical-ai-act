0fbefc174c177a6550569fff2f2bbc79a9598c91
Kristof Schroeder <kristof_schroeder@web.de>
2025-02-24 10:28:52 +0100

    Modify model factory to build pipelines for ASEC classification

--- src/asec/model_factory.py
+++ src/asec/model_factory.py
@@ -1,18 +1,21 @@
 from typing import Any
 
 from sklearn.base import BaseEstimator, ClassifierMixin
-from sklearn.pipeline import make_pipeline, Pipeline, make_union
+from sklearn.pipeline import Pipeline
 from sklearn.compose import ColumnTransformer
 from sklearn.neural_network import MLPClassifier
 from sklearn.utils.metaestimators import available_if
-from sklearn.preprocessing import LabelEncoder, OneHotEncoder, StandardScaler, \
-    OrdinalEncoder
+from sklearn.preprocessing import (
+    LabelEncoder,
+    OneHotEncoder,
+    StandardScaler,
+    OrdinalEncoder,
+)
 
 from xgboost import XGBClassifier
 from lightgbm import LGBMClassifier
 
 from .data import CensusASECMetadata
-from .features import collect_features, FeatureName
 
 
 class LabelEncodedClassifier(ClassifierMixin, BaseEstimator):
@@ -39,6 +42,13 @@ class LabelEncodedClassifier(ClassifierMixin, BaseEstimator):
         pred = self.classifier.predict(X, **params)
         return self.encoder.inverse_transform(pred)
 
+    def set_params(self, **params):
+        self.classifier.set_params(**params)
+
+    @available_if(lambda self: hasattr(self.classifier, "score"))
+    def score(self, X, y, **params):
+        return self.classifier.score(X, self.encoder.transform(y), **params)
+
     @available_if(lambda self: hasattr(self.classifier, "predict_proba"))
     def predict_proba(self, X, **params):
         return self.classifier.predict_proba(X, **params)
@@ -58,58 +68,49 @@ class ModelFactory:
     the feature union).
     """
 
+    exclude = tuple(
+        map(
+            str,
+            (
+                CensusASECMetadata.Fields.WEEKLY_EARNINGS,
+                CensusASECMetadata.Fields.HOURLY_WAGE,
+                CensusASECMetadata.Fields.LONGEST_JOB_EARNINGS,
+                CensusASECMetadata.Fields.SECOND_JOB_INCOME,
+                CensusASECMetadata.Fields.ADJUSTED_GROSS_INCOME,
+                CensusASECMetadata.Fields.ANNUAL_INCOME,
+                CensusASECMetadata.Fields.FINAL_WEIGHT,
+                CensusASECMetadata.Fields.ANNUAL_EARNINGS,
+                CensusASECMetadata.Fields.SELF_EMPLOYMENT_INCOME,
+                CensusASECMetadata.Fields.SECOND_JOB_INCOME,
+            ),
+        )
+    )
+
     @classmethod
     def create_xgb(
         cls,
-        include_only: list[FeatureName] | None = None,
-        exclude: list[FeatureName] | None = None,
-        add: list[Pipeline | ColumnTransformer] | None = None,
         **xgb_kwargs,
     ):
-        feature_union = cls._create_feature_union(include_only, exclude, add)
         classifier = LabelEncodedClassifier(XGBClassifier(**xgb_kwargs), LabelEncoder())
-        return make_pipeline(feature_union, classifier)
+        return build_pipeline(classifier, exclude=cls.exclude)
 
     @classmethod
     def create_lightgbm(
         cls,
-        include_only: list[FeatureName] | None = None,
-        exclude: list[FeatureName] | None = None,
-        add: list[Pipeline | ColumnTransformer] | None = None,
         **lgbm_kwargs,
     ):
-        feature_union = cls._create_feature_union(include_only, exclude, add)
         classifier = LabelEncodedClassifier(
             LGBMClassifier(**lgbm_kwargs), LabelEncoder()
         )
-        return make_pipeline(feature_union, classifier)
+        return build_pipeline(classifier, exclude=cls.exclude)
 
     @classmethod
     def create_mlp(
         cls,
-        include_only: list[FeatureName] | None = None,
-        exclude: list[FeatureName] | None = None,
-        add: list[Pipeline | ColumnTransformer] | None = None,
         **mlp_kwargs,
     ):
-        feature_union = cls._create_feature_union(include_only, exclude, add)
         classifier = LabelEncodedClassifier(MLPClassifier(**mlp_kwargs), LabelEncoder())
-        return make_pipeline(feature_union, classifier)
-
-    @classmethod
-    def _create_feature_union(
-        cls,
-        include_only: list[FeatureName] | None = None,
-        exclude: list[FeatureName] | None = None,
-        add: list[Pipeline | ColumnTransformer] | None = None,
-    ):
-        feature_union = collect_features(
-            include_only=include_only,
-            exclude=exclude,
-        )
-        if add is not None:
-            feature_union = make_union(*feature_union.transformer_list, *add)
-        return feature_union
+        return build_pipeline(classifier, exclude=cls.exclude)
 
 
 def build_pipeline(classifier: Any, exclude=None, encode_categoricals=True) -> Pipeline:
@@ -126,33 +127,37 @@ def build_pipeline(classifier: Any, exclude=None, encode_categoricals=True) -> P
         A scikit-learn pipeline with preprocessing and classifications steps.
     """
 
-    categorical_pipeline = Pipeline([("encoder", OneHotEncoder(handle_unknown="ignore"))])
+    categorical_pipeline = Pipeline([
+        ("encoder", OneHotEncoder(handle_unknown="ignore"))
+    ])
     numerical_pipeline = Pipeline([("scaler", StandardScaler())])
     ordinal_pipeline = Pipeline([("encoder", OrdinalEncoder())])
 
-    column_transformer = ColumnTransformer(
-        [
-            (
-                "categorical_pipeline",
-                categorical_pipeline if encode_categoricals else "passthrough",
-                CensusASECMetadata.CATEGORICAL_FEATURES,
-            ),
-            (
-                "numerical_pipeline",
-                numerical_pipeline,
-                [feat for feat in CensusASECMetadata.NUMERIC_FEATURES if not feat in exclude] if exclude is not None else CensusASECMetadata.NUMERIC_FEATURES,
-            ),
-            (
-                "ordinal_pipeline",
-                ordinal_pipeline,
-                CensusASECMetadata.ORDINAL_FEATURES,
-            ),
-        ]
-    )
+    column_transformer = ColumnTransformer([
+        (
+            "categorical_pipeline",
+            categorical_pipeline if encode_categoricals else "passthrough",
+            CensusASECMetadata.CATEGORICAL_FEATURES,
+        ),
+        (
+            "numerical_pipeline",
+            numerical_pipeline,
+            [
+                feat
+                for feat in CensusASECMetadata.NUMERIC_FEATURES
+                if not feat in exclude
+            ]
+            if exclude is not None
+            else CensusASECMetadata.NUMERIC_FEATURES,
+        ),
+        (
+            "ordinal_pipeline",
+            ordinal_pipeline,
+            CensusASECMetadata.ORDINAL_FEATURES,
+        ),
+    ])
 
-    return Pipeline(
-        [
-            ("preprocessor", column_transformer),
-            ("classifier", classifier),
-        ]
-    )
+    return Pipeline([
+        ("preprocessor", column_transformer),
+        ("classifier", classifier),
+    ])
