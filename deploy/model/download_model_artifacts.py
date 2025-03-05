5e0ef422014913d46a2d8c29a1ce1d1093a57b42
Adrian Rumpold <a.rumpold@gmail.com>
2025-03-05 10:26:12 +0100

    fix(deploy): Improve model inference container build

--- deploy/model/download_model_artifacts.py
+++ deploy/model/download_model_artifacts.py
@@ -2,12 +2,17 @@ import os
 
 import mlflow
 
-model_uri = os.getenv("MODEL_URI")
-print(f"Downloading model artifacts from {model_uri}")
-print(f"MLflow tracking URL: {os.getenv('MLFLOW_TRACKING_URI')}")
+model_name = os.getenv("MODEL_NAME")
+version = os.getenv("MODEL_VERSION")
+tracking_uri = os.getenv("MLFLOW_TRACKING_URI")
 
-# Download the model artifacts
-model_info = mlflow.models.get_model_info(model_uri)
+print(f"Downloading model artifacts for {model_name}, version {version}")
+print(f"MLflow tracking URL: {tracking_uri}")
 
-client = mlflow.MlflowClient()
-client.download_artifacts(model_info.run_id, model_info.artifact_path, "/")
+client = mlflow.MlflowClient(tracking_uri=tracking_uri, registry_uri=tracking_uri)
+if version == "latest":
+    version = client.get_latest_versions(model_name)[0].version
+    print(f"Resolved 'latest' to version {version}")
+
+download_uri = client.get_model_version_download_uri(model_name, version)
+mlflow.artifacts.download_artifacts(artifact_uri=download_uri, dst_path="/")
