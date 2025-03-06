ce10c75fd9df1c076d906eddb565b78dc831704f
Kristof Schroeder <kristof_schroeder@web.de>
2025-03-06 10:48:31 +0100

    Adapt nannyml_report script

--- scripts/nannyml_report.py
+++ scripts/nannyml_report.py
@@ -1,37 +1,48 @@
 """Create an Evidently report from the logged predictions in an SQLite3 database."""
 
 import nannyml as nml
-from asec.data import AdultData
-from config import FILE_NAME_ADULT
+from asec.data import AdultData, download_and_filter_census_data
+from asec.features import get_income_prediction_features
+from config import FILE_NAME_ADULT, FILE_NAME_ASEC
 from sklearn.model_selection import train_test_split
-
+from sensai.util.cache import pickle_cached
 import sqlite3
 from pathlib import Path
+import psycopg2
+import psycopg2.extras
 
+from income_prediction.resources.configuration import Config
 import mlflow.models
 
 from asec.nannyml import build_reference_data, load_predictions
 
-PREDICTIONS_DB_FILE = Path(__file__).parents[1] / "predictions.sqlite3"
-PREDICTIONS_TABLE = "predictions"
+db_conn = psycopg2.connect(
+    host="localhost",
+    user="postgres_user",
+    password="postgres_password",
+    dbname="hr_assistant")
+
+try:
+    with db_conn.cursor(cursor_factory=psycopg2.extras.RealDictCursor) as cur:
+        analysis_df = load_predictions(cur)
+finally:
+    db_conn.close()
 
 # NannyML reporting
 
-data = AdultData(FILE_NAME_ADULT)
-X, y = data.load_input_output_data()
+data = pickle_cached(FILE_NAME_ASEC)(download_and_filter_census_data)(year=2024)
+data = get_income_prediction_features(Config().salary_bands,data)
+X, y = data.drop(columns=["SALARY_BAND"]), data["SALARY_BAND"]
+
 _, X_test, _, y_test = train_test_split(X, y, test_size=0.2, random_state=31)
+
+mlflow.set_tracking_uri("http://localhost:50000")
 MODEL_URI = "models:/xgboost-classifier/latest"
 model = mlflow.sklearn.load_model(MODEL_URI)
 
 reference_df = build_reference_data(
     model, X_test, y_test, encoder=model.steps[-1][1].encoder
 )
-analysis_df = load_predictions(
-    sqlite3.connect(PREDICTIONS_DB_FILE),
-    PREDICTIONS_TABLE,
-    encoder=model.steps[-1][1].encoder,
-)
-
 
 chunk_size = 500
 estimator = nml.CBPE(
