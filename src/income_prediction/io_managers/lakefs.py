f25f746ec109961f18b6659d2195d38d9dc1aba5
Adrian Rumpold <a.rumpold@gmail.com>
2025-03-13 08:53:09 +0100

    fix: Read from immutable lakeFS URI in I/O manager
    This prevents potentially inconsistent reads from the branch HEAD if the
    branch has been updated since the asset was materialized.
    
    Instead, the I/O manager now reads from the immutable commit ref, which
    is extracted from the metadata of the upstream asset materialization.
    
    Closes #35

--- src/income_prediction/io_managers/lakefs.py
+++ src/income_prediction/io_managers/lakefs.py
@@ -8,7 +8,38 @@ from upath import UPath
 class LakeFSParquetIOManager(dg.UPathIOManager):
     extension = ".parquet"
 
+    @staticmethod
+    def _canonical_uri_for_output(context: InputContext) -> str | None:
+        # TODO: This only works for non-partitioned assets
+        # Once https://github.com/dagster-io/dagster/issues/20094 is resolved, we can
+        # directly access the output metadata, without having to go through the materialization
+        # events.
+        asset_key = context.asset_key
+        mat_event = context.instance.get_latest_materialization_event(asset_key)
+        meta = mat_event.dagster_event.event_specific_data.materialization.metadata
+        return str(uri.value) if (uri := meta.get("canonical_uri")) else None
+
+    def _get_path(self, context: dg.InputContext | dg.OutputContext) -> UPath:
+        if isinstance(context, dg.InputContext):
+            # Read the canonical URI from the metadata of the upstream asset materialization
+            # Reading from the raw path could cause a race condition if the repository (or
+            # rather the branch HEAD referred to in the `base_path`) has changed since the
+            # asset was materialized.
+            canonical_uri = self._canonical_uri_for_output(context)
+            if canonical_uri is None:
+                raise RuntimeError("No canonical path found in metadata")
+            return UPath(
+                canonical_uri,
+                storage_options=dict(self._base_path.storage_options),
+            )
+        else:
+            return super()._get_path(context)
+
     def load_from_path(self, context: InputContext, path: "UPath") -> pd.DataFrame:
+        # Note: this will read from the immutable commit ref, not the branch HEAD,
+        # which could lead to inconsistent data if the branch has been updated since
+        # the asset was materialized.
+        # See also the implementation of `_get_path`.
         return pd.read_parquet(path, storage_options=dict(path.storage_options))
 
     def dump_to_path(
@@ -19,17 +50,18 @@ class LakeFSParquetIOManager(dg.UPathIOManager):
             repository=repo,
             base_branch=ref,
         ) as tx:
-            path.mkdir(parents=True, exist_ok=True)
+            self.make_directory(path)
 
             # Need to add the file to the transaction branch, which gets then merged into the target branch
             ephemeral_path = f"lakefs://{repo}/{tx.branch.id}/{resource}"
             obj.to_parquet(ephemeral_path, storage_options=dict(path.storage_options))
 
+            asset_id_str = "/".join(context.get_asset_identifier())
             commit_ref = tx.commit(
-                message=f"Add data from {context.asset_key.path}",
+                message=f"Add data from {asset_id_str!r}",
                 metadata={
                     "dagster.run_id": str(context.run_id),
-                    "dagster.asset_key": str(context.asset_key.path),
+                    "dagster.asset_identifier": asset_id_str,
                 },
             )
 
