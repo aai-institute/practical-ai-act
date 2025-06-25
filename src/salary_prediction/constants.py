d42a493e00f33696345b9d7d5b2f220e7d341386
Adrian Rumpold <a.rumpold@gmail.com>
2025-06-25 09:57:37 +0200

    refactor: Extract repeated magic constant

--- src/salary_prediction/constants.py
+++ src/salary_prediction/constants.py
@@ -9,8 +9,10 @@ from .resources.configuration import (
     StratifiedShuffleCVConfig,
 )
 
+RANDOM_STATE = 495
+
 experiment_config = Config(
-    random_state=495,
+    random_state=RANDOM_STATE,
     model_name="salary-predictor",
     test_size=0.25,
     sample_fraction=0.3,
@@ -30,17 +32,18 @@ optuna_cv_config = StratifiedShuffleCVConfig(
     n_jobs=-1,
     n_splits=5,
     validation_size=0.2,
-    random_state=495,
+    random_state=RANDOM_STATE,
     scoring="f1",
 )
 
 optuna_xgb_param_distribution = OptunaXGBParamDistribution(
     classifier_prefix="estimator",
+    min_child_weight=FloatDistribution(1e-2, 1e2, log=True),
     max_depth=IntDistribution(3, 15),
-    gamma=FloatDistribution(0.0, 5.0),
-    reg_lambda=FloatDistribution(0.0, 10.0),
-    learning_rate=FloatDistribution(0.01, 0.1),
-    reg_alpha=FloatDistribution(0.0, 5.0),
+    gamma=FloatDistribution(1e-4, 1e1, log=True),
+    reg_lambda=FloatDistribution(1e-4, 10.0, log=True),
+    learning_rate=FloatDistribution(1e-3, 0.3, log=True),
+    reg_alpha=FloatDistribution(1e-4, 10.0, log=True),
 )
 
 nanny_ml_config = NannyMLConfig(
